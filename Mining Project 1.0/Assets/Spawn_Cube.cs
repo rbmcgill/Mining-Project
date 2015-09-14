7348621f4d81fa8a25f872811fbd33393104448f
Riona <riona.mcgillicuddy@gmail.com>
2015-09-14 14:10:03 -0400

    Adding Cube Script

---
+++ Mining Project 1.0/Assets/Spawn_Cube.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawn_Cube : MonoBehaviour {
+
+	public GameObject cube;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
