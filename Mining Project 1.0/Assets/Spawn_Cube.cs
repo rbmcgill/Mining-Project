67cb3b77ad4db44c85ac42793ab2a8dafbe69ca6
Riona <riona.mcgillicuddy@gmail.com>
2015-09-21 14:04:28 -0400

    Cube Spawns After 3 Seconds

--- Mining Project 1.0/Assets/Spawn_Cube.cs
+++ Mining Project 1.0/Assets/Spawn_Cube.cs
@@ -4,14 +4,20 @@ using System.Collections;
 public class Spawn_Cube : MonoBehaviour {
 
 	public GameObject cube;
-
+	bool cubeSpawned = false;
 	// Use this for initialization
 	void Start () {
-	
+		print ("It started!");
 	}
-	
 	// Update is called once per frame
 	void Update () {
-	
+		print ("It updated!");
+		if (Time.time > 3 && cubeSpawned == false) {
+			Instantiate(cube, new Vector3(0,0,0), Quaternion.identity);
+			//spawn the cube please
+			print ("Spawned a cube!");
+			cubeSpawned = true;
+			print ("Cubes stopped spawning.");
+		}
 	}
 }
