eddc7e1db0ac1fc0919f127eaa9d0b2109e63709
Riona <riona.mcgillicuddy@gmail.com>
2015-10-06 22:31:16 -0400

    Game Rules and Interaction
    Spawning gold cubes only in certain circumstances. Being able to click
    on and destroy cubes.

---
+++ Mining Project Part 3/Assets/Clicking.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Clicking : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	void OnMouseDown(){
+		Destroy (gameObject);
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Mining Project Part 3/Assets/ControlGame.cs
+++ Mining Project Part 3/Assets/ControlGame.cs
@@ -5,7 +5,19 @@ public class ControlGame : MonoBehaviour {
 	public GameObject bronzeCube;
 	public GameObject silverCube;
 	public GameObject goldCube;
-	public GameObject kyrptonite;
+	public GameObject kryptonite;
+
+	public static int bronzeCount = 0;
+	public static int silverCount = 0;
+	public static int goldCount = 0;
+	public static int kryptoniteCount = 0;
+	public static int bronzePoints = 1;
+	public static int silverPoints = 10;
+	public static int goldPoints = 100;
+	public static int kryptonitePoints = 1000;
+	public static int score = 0;
+
+	private bool recentlySpawnedGold = false;
 
 	float spawnFrequency = 1.5f;
 	float timeToAct = 0.0f;
@@ -13,6 +25,8 @@ public class ControlGame : MonoBehaviour {
 	float stopSpawningTime = 5.0f;
 	float spawnGoldTime = 20.0f;
 	float spawnKryptoniteTime = 30.0f;
+
+	GameState myState = GameState.Bronze;
 	//Ammend kyrptonite time and constraints
 
 	// Use this for initialization
@@ -23,18 +37,39 @@ public class ControlGame : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Time.time >= timeToAct && Time.time < spawnSilverTime + stopSpawningTime) {
-			Instantiate(bronzeCube, new Vector3 (Random.Range(-5f, 5f),0,Random.Range (-2f, 7f)), Quaternion.identity);
+
+		if (bronzeCount == 2 && silverCount == 2 && recentlySpawnedGold == false) {
+			myState = GameState.Gold;
+			Instantiate (goldCube, new Vector3 (Random.Range (-5f, 5f), 0, Random.Range (-2f, 7f)), Quaternion.identity);
 			timeToAct += spawnFrequency;
-			print ("A bronze cube spawned!");
+			goldCount++;
+			recentlySpawnedGold = true;
 			print (Time.time);
+			print (myState);
 		}
 
-		if (Time.time >= timeToAct && Time.time < spawnGoldTime + stopSpawningTime) {
-			Instantiate(silverCube, new Vector3 (Random.Range(-5f, 5f),0,Random.Range (-2f, 7f)), Quaternion.identity);
+		else if (Time.time >= timeToAct && Time.time < spawnSilverTime + stopSpawningTime && bronzeCount < 2) {
+			Instantiate (bronzeCube, new Vector3 (Random.Range (-5f, 5f), 0, Random.Range (-2f, 7f)), Quaternion.identity);
 			timeToAct += spawnFrequency;
-			print ("A silver cube spawned!");
+			bronzeCount++;
+			recentlySpawnedGold = false;
 			print (Time.time);
+			print (myState);
 		}
+		//If it's time to act and not yet spawning silver and there are less than 4 bronze cubes spawned:
+		//spawn the cube anywhere in this range and add the spawn frequency to time to act so the game tracks what time has passed
+
+		else if (Time.time >= timeToAct && Time.time < spawnGoldTime + stopSpawningTime && bronzeCount >= 2) {
+			myState = GameState.Silver;
+			Instantiate (silverCube, new Vector3 (Random.Range (-5f, 5f), 0, Random.Range (-2f, 7f)), Quaternion.identity);
+			timeToAct += spawnFrequency;
+			silverCount++;
+			recentlySpawnedGold = false;
+			print (Time.time);
+			print (myState);
+		} 
+
+
+	
 	}
 }
